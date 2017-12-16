4fbbc135d7cccc6f576dae270553a3735419e232
Nick <njtret0815@gmail.com>
2017-12-16 15:28:27 -0500

    add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Router } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
 import Header from './Header';
 import Footer from './Footer';
 import SpotifyContent from './SpotifyContent';
 
+const history = createBrowserHistory();
+
 export default class App extends React.Component {
   render() {
     return (
-      <div className="main-app">
-        <Header className="main-app__bar main-app__bar--header"/>
-        <SpotifyContent className="main-app__content"/>
-        <Footer className="main-app__bar main-app__bar--footer" />
-      </div>
+      <Router history={history}>
+        <div className="main-app">
+          <Header className="main-app__bar main-app__bar--header"/>
+          <SpotifyContent className="main-app__content"/>
+          <Footer className="main-app__bar main-app__bar--footer" />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router';
 
 const Content = (props) => {
   return (
     <div className="spotify-content__main">
       Welcome to the collab Q
+      <Route exact path="/" component={() => <div>Hello World home</div>}/>
+      <Route path="/songs" component={() => <div>Hello World 2</div>}/>
     </div>
   );
 };
