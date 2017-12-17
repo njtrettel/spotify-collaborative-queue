f44d3aa53ad44a59b52a211bacf25e7f1ac3753d
Nick <njtret0815@gmail.com>
2017-12-16 19:04:27 -0500

    add server

---
+++ server.js
@@ -0,0 +1,88 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const querystring = require('querystring');
+const request = require('request');
+const cookieParser = require('cookie-parser');
+
+const { clientId, clientSecret } = require('./secrets');
+
+const generateRandomString = function(length) {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+const redirectUri = 'http://localhost:1234/callback';
+
+app.use(express.static(__dirname + '/dist'));
+app.use(cookieParser());
+app.get('/', (req, res) => {
+  const loggedIn = req.cookies ? req.cookies['accessToken'] : null;
+  if (!loggedIn) {
+    res.redirect('/login');
+  } else {
+    res.sendFile('/index.html');
+  }
+});
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state
+    }));
+});
+
+app.get('/callback', (req, res) => {
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+  const code = req.query.code;
+
+  if (state === null || state !== storedState) {
+    res.status(400).send('Error: mismatched states');
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error) {
+        const accessToken = body.access_token;
+        const refreshToken = body.refresh_token;
+        res.cookie('accessToken', accessToken);
+        res.cookie('refreshToken', refreshToken);
+        res.redirect('/');
+      } else {
+        console.log(error);
+        res.status(400).send('error');
+      }
+    });
+  }
+});
+
+server.listen(1234);
+console.log('server running');
