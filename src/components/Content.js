6faf46cd16a1f0387e62d211d3462d149b72d8d1
Nick <njtret0815@gmail.com>
2017-12-17 21:21:43 -0500

    render songs table

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getSongs } from '../actions/songs';
 import _ from 'lodash';
+import Songs from './content/Songs'
 
 const actions = {
   getSongs
@@ -21,12 +22,11 @@ class Content extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    const songs = _.get(this.props, 'songs', []);
     return (
       <div className="spotify-content__main">
-        Welcome to the collab Q
         <Route exact path="/" component={() => <Redirect to="/songs" />}/>
-        <Route path="/songs" component={() => <div>Hello World 2</div>}/>
+        <Route path="/songs" component={() => <Songs songs={songs} />}/>
       </div>
     );
   }
---
+++ src/components/content/Songs.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import classnames from 'classnames';
+import _ from 'lodash';
+import { Table, Segment, Header } from 'semantic-ui-react';
+
+const renderSongsTable = (songs) => (
+  <Table basic='very' celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell><Header as='h1'>Songs</Header></Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {_.map(songs, (song, i) => (
+        <Table.Row key={i}>
+          <Table.Cell>
+            <Header as='h4'>
+              <Header.Content>
+                  {_.get(song, 'title')}
+                <Header.Subheader>{_.get(song, 'artists')}</Header.Subheader>
+              </Header.Content>
+            </Header>
+          </Table.Cell>
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
+);
+
+const Songs = (props) => {
+  const classes = classnames(props.className, 'songs');
+  const songs = _.get(props, 'songs', { error: 'Empty songs' });
+  if (songs.loading) {
+    return <Segment loading />;
+  }
+  if (!!songs.error) {
+    console.error(songs.error);
+    return <div>There was an error</div>;
+  }
+  return (
+    <div className={classes}>
+      {renderSongsTable(songs.data)}
+    </div>
+  );
+};
+
+export default Songs;
