5603090556f18705cdfce96f784e3860ca1be0be
Nick <njtret0815@gmail.com>
2017-12-17 20:41:13 -0500

    add sidebar links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Accordion } from 'semantic-ui-react'
+import { Accordion } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const panels = [{
   title: 'Playlists',
@@ -18,9 +19,9 @@ const Sidebar = (props) => {
     <div className="sidebar">
       <div className="sidebar__title"> My Library </div>
       <div className="sidebar__links">
-        <div className="sidebar__link">Songs</div>
-        <div className="sidebar__link">Artists</div>
-        <div className="sidebar__link">Albums</div>
+        <Link to="/songs" className="sidebar__link">Songs</Link>
+        <Link to="artists" className="sidebar__link">Artists</Link>
+        <Link to="albums" className="sidebar__link">Albums</Link>
       </div>
       <Accordion panels={panels} fluid exclusive={false}/>
     </div>
