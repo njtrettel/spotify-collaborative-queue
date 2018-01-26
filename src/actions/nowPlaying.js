36c64f55947205a36dea3f7e1ce6075af7832e80
Nick <njtret0815@gmail.com>
2018-01-25 21:14:55 -0500

    add actions for play/pause

--- src/actions/nowPlaying.js
+++ src/actions/nowPlaying.js
@@ -3,14 +3,30 @@ import _ from 'lodash';
 import { getCookie } from '../util';
 
 export const UPDATE_NOW_PLAYING = 'UPDATE_NOW_PLAYING';
+export const PLAY = 'PLAY';
+export const PAUSE = 'PAUSE';
 
-const updateNowPlayingAction = (song) => {
-  return {
-    type: UPDATE_NOW_PLAYING,
-    song
-  };
-};
+const updateNowPlayingAction = (song) => ({
+  type: UPDATE_NOW_PLAYING,
+  song
+});
+
+const playAction = () => ({
+  type: PLAY
+});
+
+const pauseAction = () => ({
+  type: PAUSE
+});
 
 export const updateNowPlaying = (song) => (dispatch, getState) => {
   return dispatch(updateNowPlayingAction(song));
+};
+
+export const play = () => (dispatch) => {
+  return dispatch(playAction());
+};
+
+export const pause = () => (dispatch) => {
+  return dispatch(pauseAction());
 };
--- src/reducers/nowPlaying.js
+++ src/reducers/nowPlaying.js
@@ -1,16 +1,30 @@
 import {
-  UPDATE_NOW_PLAYING
+  UPDATE_NOW_PLAYING,
+  PLAY,
+  PAUSE
 } from '../actions/nowPlaying';
 import { reduceSpotifyTrack } from '../util';
 
-const initialState = { playing: false, paused: false, song: null };
+const initialState = { playing: false, paused: false, song: {} };
 
-const nowPlaying = (state = {}, action) => {
+const nowPlaying = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NOW_PLAYING:
       return {
-        playing: state.playing,
-        paused: state.paused,
+        playing: true,
+        paused: false,
+        song: action.song
+      };
+    case PLAY:
+      return {
+        playing: true,
+        paused: false,
+        song: action.song
+      };
+    case PAUSE:
+      return {
+        playing: false,
+        paused: true,
         song: action.song
       };
     default:
